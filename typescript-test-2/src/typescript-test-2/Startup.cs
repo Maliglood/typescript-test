9844a54e69f57f4be9ccfd12a4b12bcaaa059518
Захаров Андрей <ZaharovA@tomskasu.ru>
2017-07-21 17:28:13 +0700

    Установка статических файлов на сервере; Подключение марионета.

--- typescript-test-2/src/typescript-test-2/Startup.cs
+++ typescript-test-2/src/typescript-test-2/Startup.cs
@@ -10,28 +10,23 @@ using Microsoft.Extensions.Logging;
 
 namespace typescript_test_2
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			app.UseStaticFiles();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-    }
+			app.Run(async (context) =>
+			{
+				await context.Response.WriteAsync("Page Not Found");
+			});
+		}
+	}
 }
