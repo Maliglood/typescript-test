bbb446ad0ee2c476ec3b21b5b0fa8af64d8415e7
Захаров Андрей <ZaharovA@tomskasu.ru>
2017-08-07 11:49:00 +0700

    Добавлен отдельный файл для примера импорта.

---
+++ typescript-test-2/src/typescript-test-2/Scripts/examples/importExamples.ts
@@ -0,0 +1,9 @@
+﻿// Вариант 1
+import {testViewClass} from "../views/testView";
+let testView: testViewClass = new testViewClass();
+testView.sayHello();
+
+// Вариант 2
+import * as testViewAll from "../views/testView";
+let testView2 = new testViewAll.testViewClass();
+testView2.sayTsIsCool();
--- typescript-test-2/src/typescript-test-2/app.js
+++ typescript-test-2/src/typescript-test-2/app.js
@@ -1,6 +1,7 @@
 "use strict";
 var testView_1 = require("./Scripts/views/testView");
 var testViewAll = require("./Scripts/views/testView");
+var Marionette = require("backbone.marionette.js");
 var testView = new testView_1.testViewClass();
 testView.sayHello();
 var testView2 = new testViewAll.testViewClass();
